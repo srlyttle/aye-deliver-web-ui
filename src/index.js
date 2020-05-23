cd76aed65b7fdd36f128efb4585450f4cbb7d0be
Simon Lyttle <srlyttle@hotmail.com>
2020-05-23 15:15:18 +0100

    Amplify Boilerplate configuration

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   //<React.StrictMode> // Reenable this when antd upgrades to remove dep on findDomNodes
-      <BrowserRouter>
-      <App />
-      </BrowserRouter>
-  
- // </React.StrictMode>
- ,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
+  // </React.StrictMode>
   document.getElementById('root')
 );
 
